c3a760cbd0f1f0b08efc669df34e822ed791b41a
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-11 20:35:04 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,11 +1,21 @@
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num > 1:
-    for i in range(2, num):
-        if num % i == 0:
-            print("Not Prime")
-            break
-    else:
-        print("Prime")
-else:
-    print("Not Prime")
+# if num > 1:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("Not Prime")
+#             break
+#     else:
+#         print("Prime")
+# else:
+#     print("Not Prime")
+
+   
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+num = int(input("Enter number: "))
+print("Factorial =", factorial(num))  
