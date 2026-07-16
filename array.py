dac2a2f59ff676c015d55a2945a877cf4b3e1544
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-16 19:47:17 +0530

    Add files via upload

--- array.py
+++ array.py
@@ -15,3 +15,19 @@ if key in arr:
     print("Element found.")
 else:
     print("Element not found.")
+
+
+
+arr = [10, 15, 20, 25, 30]
+
+even = 0
+odd = 0
+
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even =", even)
+print("Odd =", odd)
