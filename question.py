23a6a063f27bf9768ac357ea872943d1f8c63eae
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-05 21:15:26 +0530

    Add files via upload

--- question.py
+++ question.py
@@ -73,3 +73,19 @@ while low <= high:
         high = mid - 1
 else:
     print("Not Found")
+
+
+string = input("Enter string: ")
+
+vowels = 0
+consonants = 0
+
+for ch in string.lower():
+    if ch.isalpha():
+        if ch in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Vowels =", vowels)
+print("Consonants =", consonants)
