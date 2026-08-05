a6b868223f00ac51d326e29f3f328c744a004332
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-05 21:11:24 +0530

    Add files via upload

--- question.py
+++ question.py
@@ -22,3 +22,19 @@ for char in text:
         frequency[char] = 1
 
 print(frequency)
+
+
+
+A = [[1,2,3],
+     [4,5,6]]
+
+B = [[7,8,9],
+     [1,2,3]]
+
+result = [[0,0,0],[0,0,0]]
+
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        result[i][j] = A[i][j] + B[i][j]
+
+print(result)
